7a2de39a7984e8707acf4c6f8f080a544cc589ed
YoungKyonYou <man.spride1324@gmail.com>
2021-09-22 15:10:11 +0900

    ClassPresident problem

--- Arrays/ClassPresident.java
+++ Arrays/ClassPresident.java
@@ -4,42 +4,43 @@ public class  ClassPresident{
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] arr = new int[N][N];
+        int[][] arr = new int[N][5];
         for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < 5; j++)
                 arr[i][j] = sc.nextInt();
         System.out.println(solution(arr));
     }
 
     public static int solution(int[][] arr) {
         int max = 0;
-        int[] ans=new int[arr.length];
+        int[][] flag = new int[arr.length][arr.length];
         int idx = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int k = 0; k < arr.length; k++) {
-                for (int j = k; j < arr.length - 1; j++) {
-                    System.out.println("arr[j][k]:" + arr[j][k] + " arr[j+1][k]:" + arr[j + 1][k]);
-                    System.out.println("test:" + arr[1][0]);
-                    if (arr[j][k] == arr[j + 1][k]) {
-                        ans[i]++;
+            for (int k = 0; k < 5; k++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i][k] == arr[j][k] && flag[i][j] == 0) {
+                        flag[i][j] = 1;
                     }
                 }
-                for (int j = k; j >0; j--) {
-                    if (arr[j][k] == arr[j - 1][k]) {
-                        ans[i]++;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (arr[i][k] == arr[j][k] && flag[i][j] == 0) {
+                        flag[i][j] = 1;
                     }
                 }
             }
 
         }
-        for (int i = 0; i < ans.length; i++) {
-            if (max < ans[i]) {
-                max = ans[i];
+        for(int i=0;i<flag.length;i++){
+            int sum=0;
+            for (int j = 0; j < arr.length; j++) {
+                if(flag[i][j]==1)
+                    sum++;
+            }
+            if (max < sum) {
+                max = sum;
                 idx = i;
             }
         }
-        System.out.println(Arrays.toString(ans));
-
         return idx+1;
     }
 }
